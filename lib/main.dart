edf60dd92830f0e04dde6bbbbe731e02139eb5df
jenil82 <jenil82>
2023-03-30 10:46:13 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google/screen/home/provider/google_provider.dart';
+import 'package:google/screen/home/view/google_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => HomeProvider(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => HomeScreen(),
+        },
+      ),
+    ),
+  );
+}
